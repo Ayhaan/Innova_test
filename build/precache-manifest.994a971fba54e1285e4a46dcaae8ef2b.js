2efbac4706fa1ac02a9f4344f9e614b3ce1a8afd
Ayhan <caliskanayhan@hotmail.frgit config --global user.naimeayhangit config --global user.email caliskanayhan@hotmail.fr>
2020-08-10 15:37:25 +0200

    ajout du dossier build

---
+++ build/precache-manifest.994a971fba54e1285e4a46dcaae8ef2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2432bcadad6d553657b430641aed18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "462e0196e7047b91b5cf",
+    "url": "/static/js/2.606bc9fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.606bc9fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fb35e1045c6c6c1e048",
+    "url": "/static/js/main.e87d1255.chunk.js"
+  },
+  {
+    "revision": "b47b0d2c6645833b9069",
+    "url": "/static/js/runtime-main.e1580744.js"
+  }
+]);
